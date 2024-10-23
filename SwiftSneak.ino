7a2e5f3d5485e2ed1f29ee442ee7d588990fd26a
Botolog <81357028+Botolog@users.noreply.github.com>
2024-10-23 13:34:10 +0300

    motors is vroom vrooming

--- SwiftSneak.ino
+++ SwiftSneak.ino
@@ -4,10 +4,10 @@ bool EN_MOTORS = false, EN_DISPLAY = false, EN_RGB = false, EN_GYRO = false, EN_
 #define PinRFminus 17
 #define PinLFplus 15
 #define PinLFminus 16
-#define PinRBplus 23
-#define PinRBminus 14
-#define PinLBplus 12
-#define PinLBminus 13
+#define PinRBplus 14
+#define PinRBminus 23
+#define PinLBplus 13
+#define PinLBminus 12
 
 #define RFplus 2
 #define RFminus 3
@@ -21,8 +21,7 @@ bool EN_MOTORS = false, EN_DISPLAY = false, EN_RGB = false, EN_GYRO = false, EN_
 const int freq = 3000;
 const int resolution = 8;
 
-void setupMotors()
-{
+void setupMotors() {
   EN_MOTORS = true;
   ledcSetup(RFplus, freq, resolution);
   ledcSetup(RFminus, freq, resolution);
@@ -42,47 +41,39 @@ void setupMotors()
   ledcAttachPin(PinLBminus, LBminus);
 }
 
-void SetSpeed(bool right, bool front, float speed = 0)
-{
-  speed = max(min((speed + 100) / 200 * 255, 255.0F), 0.0F);
+void SetSpeed(bool right, bool front, double speed = 0) {
+  Serial.print(speed); Serial.print(" ==> ");
+  speed = max(min(((speed) + 100) / 200 * 255, 255.0), 0.0);
+  Serial.println(speed);
 
-  if (right && front)
-  {
+  if (right && front) {
     ledcWrite(RFplus, speed);
     ledcWrite(RFminus, 255 - speed);
-  }
-  else if (right && !front)
-  {
+  } else if (right && !front) {
     ledcWrite(RBplus, speed);
     ledcWrite(RBminus, 255 - speed);
-  }
-  else if (!right && front)
-  {
+  } else if (!right && front) {
     ledcWrite(LFplus, speed);
     ledcWrite(LFminus, 255 - speed);
-  }
-  else if (!right && !front)
-  {
+  } else if (!right && !front) {
     ledcWrite(LBplus, speed);
     ledcWrite(LBminus, 255 - speed);
   }
 }
 
-double B(double x)
-{
-  min(100, max(-100, x));
+double B(double x) {
+  return min(100.0, max(-100.0, x));
 }
 
-void drive(double forward, double right, double turn)
-{
-  SetSpeed(false, true, B((forward + right + turn) / (abs(forward) + abs(right) + abs(turn))));
-  SetSpeed(true, true, B((forward - right - turn) / (abs(forward) + abs(right) + abs(turn))));
-  SetSpeed(false, false, B((forward - right + turn) / (abs(forward) + abs(right) + abs(turn))));
-  SetSpeed(true, false, B((forward + right - turn) / (abs(forward) + abs(right) + abs(turn))));
+void drive(double forward, double right, double turn) {
+  forward /= 100; right /= 100; turn /= 100;
+  SetSpeed(false, true,  100*B(forward + right + turn));
+  SetSpeed(true, true,   100*B(forward - right - turn));
+  SetSpeed(false, false, 100*B(forward - right + turn));
+  SetSpeed(true, false,  100*B(forward + right - turn));
 }
 
-void stop()
-{
+void stop() {
   ledcWrite(RFplus, 0);
   ledcWrite(RFminus, 0);
   ledcWrite(RBplus, 0);
@@ -93,291 +84,39 @@ void stop()
   ledcWrite(LBminus, 0);
 }
 
-//* Servos
-// #include <Servo.h>
-
-Servo Xservo;
-Servo Yservo;
-
-void setupServo()
-{
-  EN_SERVO = true;
-  pinMode(25, OUTPUT);
-  Xservo.attach(23);
-  Yservo.attach(2);
-  Yservo.write(0);
-  Xservo.write(0);
-}
-
-int XservoPos = 0, YservoPos = 0;
-
-void targetServo(int Xtarget, int Ytarget)
-{
-  while (abs(Xtarget - XservoPos) + abs(Ytarget - YservoPos) > 2)
-  {
-    int dx = abs(Xtarget - XservoPos) / (Xtarget - XservoPos);
-    int dy = abs(Ytarget - YservoPos) / (Ytarget - YservoPos);
-    Xservo.write(XservoPos + dx);
-    XservoPos = XservoPos + dx;
-    Yservo.write(YservoPos + dy);
-    YservoPos = YservoPos + dy;
-    delay(20);
-  }
-}
-
-void scan()
-{
-  String toSend[180];
-  Xservo.write(XServo_position * K);
-  for (int YServo_position = 0; YServo_position < 180; YServo_position += dy)
-  {
-    if (true)
-    { //(XServo_position < 90){
-      for (XServo_position = 0; XServo_position < 180; XServo_position += dx)
-      {
-        Xservo.write(XServo_position * K);
-        xdots[XServo_position] = XServo_position;
-      }
-    }
-    else
-    {
-      for (XServo_position = 180; XServo_position > 0; XServo_position -= dx)
-      {
-        Xservo.write(XServo_position * K);
-        xdots[XServo_position] = mesure();
-      }
-    }
-    Yservo.write(YServo_position);
-    Xservo.write(0);
-    // uploadScan(YServo_position, xdots); 
-    // TODO uploadScan(YServo_position, xdots);
-    String toAdd = "";
-
-    delay(20);
-  }
-  
-}
-
-//* WiFi & HTTP
-// #include <WiFi.h>
-// #include <HTTPClient.h>
-
-const char *ssid = "Kaluga 2.4";
-const char *password = "41931047";
-
-WiFiServer server(80);
-
-String BaseURL = "";
-
-void setupWiFi()
-{
-  EN_WIFI = true;
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-  Serial.println("");
-  Serial.print("WiFi connected: ");
-  Serial.println(WiFi.localIP());
-  if (EN_DISPLAY)
-  {
-    printOnDisplay(String(WiFi.localIP()));
-  }
-}
-
-int contX;
-int contY;
-int camX;
-int camY;
-int option;
-
-void sep(String a)
-{
-  a.remove(0, 1);
-  a.remove(a.length() - 1);
-
-  // Split the string by ","
-  int commaIndex1 = a.indexOf(',');
-  String b = a.substring(0, commaIndex1); // Extract substring before the first comma
-
-  // Find the second comma after the first one
-  int commaIndex2 = a.indexOf(',', commaIndex1 + 1);
-  String c = a.substring(commaIndex1 + 1, commaIndex2); // Extract substring between first and second comma
-
-  // Find the third comma after the second one
-  int commaIndex3 = a.indexOf(',', commaIndex2 + 1);
-  String d = a.substring(commaIndex2 + 1, commaIndex3); // Extract substring between second and third comma
-
-  // Find the fourth comma after the third one
-  int commaIndex4 = a.indexOf(',', commaIndex3 + 1);
-  String e = a.substring(commaIndex3 + 1, commaIndex4); // Extract substring between third and fourth comma
-  String f = a.substring(commaIndex4 + 1);              // Extract substring after the fourth comma
-
-  contX = b.toInt();
-  contY = c.toInt();
-  camX = d.toInt();
-  camY = e.toInt();
-  option = f.toInt();
-}
-
-void remoteCtrl()
-{
-  server.begin();
-  String command = "N";
-  WiFiClient client = server.available();
-  Serial.println("rmtctrl started");
-  if (client)
-  {
-    Serial.println("New client connected");
-    while (client.connected() || command == "/")
-    {
-      if (client.available())
-      {
-        command = client.readStringUntil(';');
-        if (command.indexOf("/") != -1)
-        {
-          Serial.println("Got IP: " + command);
-          BaseURL = command;
-        }
-        Serial.println("Received command: " + command);
-        sep(command);
-        int speed = contY, shift = contX;
-        if (speed != 0 || shift != 0)
-        {
-          drive(speed, 0, shift);
-        }
-        else
-        {
-          stop();
-        }
-        if (EN_SERVO)
-        {
-          targetServo(camX, camY);
-        }
-        if (option == 1)
-        {
-          digitalWrite(25, HIGH);
-        }
-        else if (option == 0)
-        {
-          digitalWrite(25, LOW);
-        }
-        else if (option > 1)
-        {
-          scan();
-        }
-      }
-    }
-  }
-  Serial.println("Client disconnected");
-}
-
-void setupScanner()
-{
-  EN_SCANNER = true;
-  while (0)
-  {
-    delay(1500);
-    for (int test = 0; test <= 180; test += 1)
-    {
-      delay(10);
-      Xservo.write(test * K);
-      Serial.println(test);
-    }
-    delay(500);
-    Xservo.write(0);
-    delay(500);
-  }
-}
-
-int16_t mesure()
-{
-  if (!EN_SCANNER)
-  {
-    Serial.println("pls turn on scanner!!");
-    ESP.restart();
-  }
-  delay(10);
-  tflI2C.getData(tfDist, tfAddr);
-  Serial.println(tfDist);
-  return tfDist;
-}
-
-int XServo_position = 0;
-int dx = 1;
-int dy = 1;
-double t = 1.2;
-
-void scan()
-{
-  String toSend[180];
-  Xservo.write(XServo_position * K);
-  for (int YServo_position = 0; YServo_position < 180; YServo_position += dy)
-  {
-    if (true)
-    { //(XServo_position < 90){
-      for (XServo_position = 0; XServo_position < 180; XServo_position += dx)
-      {
-        Xservo.write(XServo_position * K);
-        xdots[XServo_position] = XServo_position;
-      }
-    }
-    else
-    {
-      for (XServo_position = 180; XServo_position > 0; XServo_position -= dx)
-      {
-        Xservo.write(XServo_position * K);
-        xdots[XServo_position] = mesure();
-      }
-    }
-    Yservo.write(YServo_position);
-    Xservo.write(0);
-    uploadScan(YServo_position, xdots);
-    String toAdd = "";
-
-    delay(20);
-  }
-  
-}
-
-void uploadScan(int pos, int theScan[180])
-{
-  HTTPClient http;
-  String Line = "";
-    Line += theScan[i] + ",";
-  }
-  Line += theScan[179];
-
-    String url = BaseURL + "US/" + pos + "/" + Line;
-    http.begin(url);
-    int httpResponseCode = http.GET();
-    http.end();
-}
 
 //* Main
-void setup()
-{
-  Serial.begin(115200);
+void setup() {
   setupMotors();
-  setupServo();
-  // setupDisplay();
-  // setupRGB();
-  // setupGyro();
-  // setupIR();
-  setupWiFi();
-  setupScanner();
-
+  // stop();
+  Serial.begin(115200);
   Serial.println("Ready! Starting...");
   delay(500);
 }
 
-void loop()
-{
-  Serial.println("starting rmtctrl...");
-  remoteCtrl();
-  Serial.println("rmtctrl ended");
-  delay(5000);
-  // driveIR(90, 5000, 0.3, 0.08);
+void loop() {
+  Serial.println("startDrive");
+  Serial.println(abs(-1));
+  Serial.println(B(10.0));
+  delay(2000);
+  Serial.println("===============");
+  drive(50.0, 0, 0);
+  delay(1000);
+  Serial.println("===============");
+  drive(0, 50.0, 0);
+  delay(1000);
+  Serial.println("===============");
+  drive(0, 0, 50.0);
+  delay(1000);
+  Serial.println("===============");
+  drive(0, 0, -50.0);
+  delay(1000);
+  Serial.println("===============");
+  drive(0, -50.0, 0);
+  delay(1000);
+  Serial.println("===============");
+  drive(-50, 0, 0);
+  delay(1000);
+  Serial.println("===============");
+  stop();
 }
